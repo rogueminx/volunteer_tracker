3d39b0209352fa39e7b569654314b0e852c209b8
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-02-23 12:21:12 -0800

    allow user to update project title online.

--- app.rb
+++ app.rb
@@ -12,10 +12,27 @@ get("/") do
   erb(:index)
 end
 
+get("/projects/:id") do
+  @current_project = Project.find(params.fetch("id").to_i())
+  erb(:projectinfo)
+end
+
+get("/projects/:id/edit") do
+  @current_project = Project.find(params.fetch("id").to_i())
+  erb(:editproject)
+end
+
 post("/addproject") do
   title = params.fetch("title")
   project = Project.new({:title => title, :id => nil})
   project.save()
   @projects = Project.all()
   erb(:index)
+end
+
+patch("/projects/:id") do
+  title = params.fetch("title")
+  @current_project = Project.find(params.fetch("id").to_i())
+  @current_project.update({:title => title})
+  erb(:projectinfo)
 end
